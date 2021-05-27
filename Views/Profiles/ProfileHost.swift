66d3d6eca1ec7b00e6f23321bcfd27cfac981684
Gammelsmxlf <oizaccounts@me.com>
2021-05-27 14:44:51 +0200

    Profile in now editable - Limited fields

--- Views/Profiles/ProfileHost.swift
+++ Views/Profiles/ProfileHost.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onAppear {
+                        modelData.profile = draftProfile
+                    }
+        
+            }
+            
         }
         .padding()
     }
@@ -21,5 +46,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
