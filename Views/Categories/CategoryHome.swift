b4267f1e37edced18de284001d8af22f3bc07918
Gammelsmxlf <oizaccounts@me.com>
2021-05-28 17:34:21 +0200

    Introduced nice carousel for featured landmarks

--- Views/Categories/CategoryHome.swift
+++ Views/Categories/CategoryHome.swift
@@ -15,30 +15,27 @@ struct CategoryHome: View {
         
         NavigationView {
             List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    .aspectRatio(3 / 2, contentMode: .fit)
                     .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
-                            }
-                            .listStyle(InsetListStyle())
-                            .navigationTitle("Featured")
-                            .toolbar {
-                                Button(action: { showingProfile.toggle() }) {
-                                    Image(systemName: "person.crop.circle")
-                                        .accessibilityLabel("User Profile")
-                                }
-                            }
-                            .sheet(isPresented: $showingProfile) {
-                                ProfileHost()
-                                    .environmentObject(modelData)
-                            }
+                }
+                .listStyle(InsetListStyle())
+                .navigationTitle("Featured")
+                .toolbar {
+                    Button(action: { showingProfile.toggle() }) {
+                        Image(systemName: "person.crop.circle")
+                            .accessibilityLabel("User Profile")
+                        }
+                    }
+                .sheet(isPresented: $showingProfile) {
+                    ProfileHost()
+                        .environmentObject(modelData)
+                }
         }
     }
 }
